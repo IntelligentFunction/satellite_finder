4c625f7db1ccef49889464dbf083be2592a073df
Robert Kinsey <intelligent.function@gmail.com>
2018-07-04 10:40:56 +0100

    Tidy code in sats_api, move div tag from index.html to sats_api.js and list Satallites in nested lists

--- sats_api.js
+++ sats_api.js
@@ -18,40 +18,41 @@ function load(url, callback) {
         break;
       }
       catch(e) {}
-    } // end for
+    }
   }
 
-xhr.onreadystatechange = loadCallback;
-
-function loadCallback() {
-  if (xhr.readyState < 4) {
-    return;
+  xhr.open('GET', requestURL, true);
+  xhr.send('');
+  xhr.onload = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      callback(xhr);
+    }
   }
-  if (xhr.status !== 200) {
-    return;
-  }
-  // request parsed OK
-  if (xhr.readyState === 4) {
-    callback(xhr);
-  }
-}
-xhr.open('GET', requestURL, true);
-xhr.send('');
 }
 
 // 'Find Satellites' button calls this function
 function getSats() {
-load(requestURL, function(xhr) {
+  load(requestURL, function(xhr) {
   var data = xhr.responseText;
   var json = JSON.parse(data);
   console.log(data);
-  alert("There are currently "+json.info.satcount+" Satellites above pin location \nwithin a 10 degree range. \nClick ok and scroll down to see Satallite list.")
 
+  alert("There are currently "+json.info.satcount+" Satellites above pin location \nwithin a 10 degree range. \nClick ok and scroll down to see Satallite list.")
   // Create a list out of Satallite names to appear below button
-  var output = document.getElementById('output');
-  output.innerHTML = '<h2>Here are the list of Satallite names</h2>'
+  var output = document.getElementById('sats');
+  output.innerHTML = '<h2>Here are the list of Satallite names</h2>'+'<div>' +  '<ol>'
   for (var i = 0; i < json.info.satcount; i++) {
-    output.innerHTML += '<li>'+json.above[i].satname+'</li>'
+    output.innerHTML +=
+    '<li>' +
+    '<ul>' +
+    '<li>Sat ID: '+json.above[i].satid+'</li>' +
+    '<li>Sat Name: '+json.above[i].satname+'</li>' +
+    '<li>Sat LaunchDate '+json.above[i].launchDate+'</li>' +
+    '</ul>' +
+    '<br>' +
+    '</li>' +
+    '</ol>'
     }
+    '</div>'
   });
 }
